a5559c4e82a8fabcf0d39425f684712dca6abfac
chenyibo <chenyibo2@jd.com>
2019-08-28 16:45:49 +0800

    update shell log

--- jd/ganesha_log_conf.sh
+++ jd/ganesha_log_conf.sh
@@ -2,12 +2,39 @@
 
 set -x
 
-echo "1.config logrotate of ganesha"
-if [ -f "/etc/logrotate.d/ganesha" ]
-then
-    echo "ganesh config file exist"
-else
-    echo "NOT EXIST! now add ganesha's logrotate conf"
+function log_info() {
+    if [ ! -d /export/jcloud-cfs/log ]
+    then
+        mkdir -p /export/jcloud-cfs/log
+    fi
+
+    DATE_N=`date "+%Y-%m-%d %H:%M:%S"`
+    USER_N=`whoami`
+    echo "${DATE_N} ${USER_N} execute $0 [INFO] $@" >> /export/jcloud-cfs/log/op.log
+}
+
+function log_error() {
+    DATE_N=`date "+%Y-%m-%d %H:%M:%S"`
+    USER_N=`whoami`
+    echo -e "\033[41;37m ${DATE_N} ${USER_N} execute $0 [ERROR] $@ \033[0m"  >> /export/jcloud-cfs/log/op.log
+}
+
+function fn_log()  {
+    cmd = $@
+    re = `${cmd}`
+    if [  re -eq 0  ]
+    then
+        log_info "$@ sucessed."
+        echo -e "\033[32m $@ sucessed. \033[0m"
+    else
+        log_error "$@ failed."
+        echo -e "\033[41;37m $@ failed. \033[0m"
+        exit 1
+    fi
+}
+
+function set_ganesha_logrotate() {
+    log_info "1.config logrotate of ganesha"
     cat  <<EOF > /etc/logrotate.d/ganesha
 /export/jcloud-cfs/log/ganesha*.log {
     rotate 60
@@ -15,21 +42,18 @@ else
     compress
     missingok
     notifempty
-    copytruncate
 }
 EOF
-fi
+}
 
-echo "2.config cron task of ganesha"
-if [ -f "/etc/cron.d/ganesha" ]
-then
-    echo "ganesh cron task has been setted"
-else
-    echo "NOT EXIST! now set cron task"
+function set_crontable() {
+    log_info "2.config cron task of ganesha"
     echo "*/5 * * * * root /usr/sbin/logrotate /etc/logrotate.d/ganesha" > /etc/cron.d/ganesha
-fi
+}
 
-cat /etc/logrotate.d/ganesha
-cat /etc/cron.d/ganesha
-echo "success"
-
+###main
+log_info "You are setting rotation of ganesha log, which is idempotent."
+set_ganesha_logrotate
+set_crontable
+log_info "Finished"
+log_info "~~~~~~~~~~~~~~~~~~~"
